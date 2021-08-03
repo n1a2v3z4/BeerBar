7b783141b73080e1a2ffd8c8addc4f789dcb6dc1
nik1olay <85620548+nik1olay@users.noreply.github.com>
2021-08-03 16:36:46 +0300

    Initial Commit

---
+++ Bear/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bear
+//
+//  Created by Cергей Иванович on 3.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
